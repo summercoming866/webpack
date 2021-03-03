65238221fd46821f5f4fba4dcac46957d6796bd6
lfy <438467132@qq.com>
2021-03-03 23:15:49 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
