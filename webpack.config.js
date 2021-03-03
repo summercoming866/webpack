e5dc994fce7a65a930ad953ddb8cc93dc14c3f25
lfy <438467132@qq.com>
2021-03-03 22:46:43 +0800

    update

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "66's App",
+      template: 'src/assets/index.html'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
